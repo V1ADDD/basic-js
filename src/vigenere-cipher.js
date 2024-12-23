e9b55e3d3d3761a217d0d39cfac046c8689c2e22
V1ADDD <rusakvlad2@gmail.com>
2024-12-23 10:56:58 +0300

    feat: vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse = true) {
+    this.reverse = reverse;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(text, code) {
+    if (arguments.length < 2 || [...arguments].some((a) => typeof a !== 'string')) throw new Error('Incorrect arguments!');
+    text = text.toUpperCase();
+    const textAr = text.split('');
+    const numCode = code.split('').map((a) => this.alphabet.search(a.toUpperCase()));
+    let index = 0;
+    const cypheredText = textAr.map((a) => {
+      let indexA = a === '.' 
+        ? -1 
+        : typeof a === 'string' && this.alphabet.includes(a.toUpperCase()) 
+        ? this.alphabet.indexOf(a.toUpperCase()) 
+        : -1;
+      if (indexA >= 0) {
+        indexA += numCode[index % numCode.length];
+        index += 1;
+        return this.alphabet[indexA % 26];
+      }
+      return a;
+    })
+    if (!this.reverse)
+      return cypheredText.reverse().join('');
+    return cypheredText.join('');
+  }
+  decrypt(text, code) {
+    if (arguments.length < 2 || [...arguments].some((a) => typeof a !== 'string')) throw new Error('Incorrect arguments!');
+    text = text.toUpperCase();
+    const textAr = text.split('');
+    const numCode = code.split('').map((a) => this.alphabet.search(a.toUpperCase()));
+    let index = 0;
+    const decriptedText = textAr.map((a) => {
+      let indexA = a === '.' 
+        ? -1 
+        : typeof a === 'string' && this.alphabet.includes(a.toUpperCase()) 
+        ? this.alphabet.indexOf(a.toUpperCase()) 
+        : -1;
+      if (indexA >= 0) {
+        indexA -= numCode[index % numCode.length];
+        index += 1;
+        return this.alphabet[(indexA + 26) % 26];
+      }
+      return a;
+    })
+    if (!this.reverse)
+      return decriptedText.reverse().join('');
+    return decriptedText.join('');
   }
 }
 
